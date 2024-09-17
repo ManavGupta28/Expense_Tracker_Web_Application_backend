56d259c6e1e2db26b1625d8e42a97410f25cb2d0
Manav Gupta <gmanav405@gmail.com>
2024-09-17 15:45:48 +0530

    hii

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ const ensureAuthenticated = require("./Middlewares/Auth");
 require("dotenv").config();
 require("./Models/db");
 const PORT = process.env.PORT;
-app.get("/" ,(req,res)=>{
-  console.log("Hello World");
-})
+
 app.get("/ping", (req, res) => {
   res.send("PONG");
 });
